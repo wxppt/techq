b13a20fd99a0ca3004176edf46b11e027cf1f4eb
wxppt <wx_ppt@126.com>
2014-10-28 08:08:06 +0800

    user的增加和修改逻辑

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -48,6 +48,7 @@ class UserController extends BaseController {
         $user->role = 0;
         $user->headpic = "default";
         $user->intro = "";
+        $user->status = 0;
 
         if($user->save() == true) {
             $this->session->set('auth', array(
@@ -57,10 +58,6 @@ class UserController extends BaseController {
             echo json_encode(array('fbCode' => 1, 'message' => '注册成功' ));
         } else {
             echo json_encode(array('fbCode' => -1, 'message' => '注册失败' ));
-            // echo "Sorry, the following problems were generated:\n";
-            // foreach ($user->getMessages() as $message) {
-            //     echo $message->getMessage(), "\n";
-            // }
         }
     }
 
@@ -77,6 +74,64 @@ class UserController extends BaseController {
             
         } else {
             echo json_encode(array('fbCode' => -1, 'message' => '找不到UID为'.$uid.'的用户'));
+        }
+    }
+
+    public function addAction() {
+        $email = $this->request->getpost("email","email");
+        $password = $this->rquest->getpost("password");
+        $nickname = $this->request->getpost("nickname","string");
+        $role = $this->request->getpost("role","int");
+        $points = $this->request->getpost("points","int");
+        $status = $this->request->getpost("status","int");
+
+        $user = new User();
+        $user->email = $email;
+        $user->password = md5($email ." ".$password);
+        $user->nickname = $nickname;
+        $user->time = date('Y-m-d H:i:s');
+        $user->points = $points;
+        $user->role = $role;
+        $user->status = $status;
+        $user->headpic = "default";
+        $user->intro = "";
+
+        if($user->save() == true) {
+            $this->session->set('auth', array(
+                'uid' => $user->uid,
+                'role' => $user->role
+            ));
+            echo json_encode(array('fbCode' => 1, 'message' => '增加成功' ));
+        } else {
+            echo json_encode(array('fbCode' => -1, 'message' => '增加失败' ));
+        }
+    }
+
+    public function updateAction() {
+        $uid = $this->request->getpost("uid","int");
+        $email = $this->request->getpost("email","email");
+        $password = $this->rquest->getpost("password");
+        $nickname = $this->request->getpost("nickname","string");
+        $role = $this->request->getpost("role","int");
+        $points = $this->request->getpost("points","int");
+        $status = $this->request->getpost("status","int");
+
+        $user = User::findFirst("uid='$uid'");
+        $user->email = $email;
+        $user->password = md5($email ." ".$password);
+        $user->nickname = $nickname;
+        $user->points = $points;
+        $user->role = $role;
+        $user->status = $status;
+
+        if($user->save() == true) {
+            $this->session->set('auth', array(
+                'uid' => $user->uid,
+                'role' => $user->role
+            ));
+            echo json_encode(array('fbCode' => 1, 'message' => '修改成功' ));
+        } else {
+            echo json_encode(array('fbCode' => -1, 'message' => '修改失败' ));
         }
     }
 }
